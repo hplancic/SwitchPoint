606b3c9df95ce26e4b4ba877930329a0d23c92e1
Filip Crnoja <fc55256@fer.hr>
2024-10-30 16:27:54 +0100

    rad na početnoj stranici

---
+++ react/src/CardList.jsx
@@ -0,0 +1,22 @@
+import Card from './Card'
+
+function CardList() {
+    return (
+        <div className='card-list'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default CardList
