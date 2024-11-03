a780b64feb636780ba4c1d8d73688c65963f7d6d
Filip Crnoja <fc55256@fer.hr>
2024-11-03 16:33:10 +0100

    promjena gumba sa <a> tagom u <Link> radi bržeg loadanja stranica

--- Frontend/react/src/Header.jsx
+++ Frontend/react/src/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return (
         <div className="header">
             <Link to="/" className="header-title">SwitchPoint</Link>
-            <div className="header-buttons">
+            <div className="header-buttons"> 
                 <Link to="/login" className="header-button">Log in</Link>
                 <Link to="/signup" className="header-button">Sign up</Link>
             </div>
