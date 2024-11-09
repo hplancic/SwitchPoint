3121bd21b0e0570366f9d8ffb9ff6d1b18117ca8
Sciller4 <mt55003@fer.hr>
2024-11-09 13:49:12 +0100

    Lista ploča se automatski generira iz liste

--- Frontend/react/src/Card.jsx
+++ Frontend/react/src/Card.jsx
@@ -1,40 +1,36 @@
 import { Link } from 'react-router-dom'
 import './Card.css'
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
-            <img className="albumImage" src="/Prokletije2Front.jpg" />
+            <img className="albumImage" src= { props.vinyl.vinylImage} />
             <div className="KVPair">
                 <div className="key">Izvođač</div>
-                <div className="value">Nemeček</div>
+                <div className="value"> {props.vinyl.artist}</div>
             </div>
             <div className="KVPair">
                 <div className="key">Album</div>
-                <div className="value">Prokletije II</div>
+                <div className="value"> {props.vinyl.vinylTitle} </div>
             </div>
             <div className="KVPair">
                 <div className="key">Žanr</div>
-                <div className="value bubble">Rock</div>
+                <div className="value bubble"> {props.vinyl.genre} </div>
             </div>
 
             <hr></hr>
 
             <div className="KVPair">
-                <div className="key">Oznaka izdanja</div>
-                <div className="value">V-8538</div>
-            </div>
-            <div className="KVPair">
                 <div className="key">Godina Izdanja</div>
-                <div className="value bubble">2024</div>
+                <div className="value bubble"> {props.vinyl.releaseYear} </div>
             </div>
             <div className="KVPair">
                 <div className="key">Stanje Ploče</div>
-                <div className="value bubble">VG+</div>
+                <div className="value bubble"> {props.vinyl.vinylCondition} </div>
             </div>
             <div className="KVPair">
                 <div className="key">Stanje Omota</div>
-                <div className="value bubble">NM</div>
+                <div className="value bubble"> {props.vinyl.sleeveCondition} </div>
             </div>
 
             <hr></hr>
--- Frontend/react/src/CardList.jsx
+++ Frontend/react/src/CardList.jsx
@@ -1,19 +1,41 @@
 import Card from './Card'
 
+const fetchedArray = [
+    {
+        vinylId: 1,
+        vinylTitle: "Prokletije II",
+        artist: "Nemeček",
+        genre: "Rock",
+        releaseYear: "2024",
+        vinylImage: "/Prokletije2Front.jpg",
+        username: "Marijanus",
+        sleeveCondition: "M",
+        vinylCondition: "M"
+    },
+    {
+        vinylId: 2,
+        vinylTitle: "Prokletije",
+        artist: "Nemeček",
+        genre: "Rock",
+        releaseYear: "2021",
+        vinylImage: "/Prokletije2Back.jpg",
+        username: "Marijanus",
+        sleeveCondition: "P",
+        vinylCondition: "VG+"
+    }
+]
+
 function CardList() {
     return (
         <div className='card-list'>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            { fetchedArray.map((item) => <Card vinyl={ item } key={ item.vinylId}/>) }
+            <Card vinyl= { fetchedArray[0]}/>
+            <Card vinyl= { fetchedArray[0]}/>
+            <Card vinyl= { fetchedArray[0]}/>
+            <Card vinyl= { fetchedArray[0]}/>
+            <Card vinyl= { fetchedArray[0]}/>
+            <Card vinyl= { fetchedArray[0]}/>
+            <Card vinyl= { fetchedArray[0]}/>
         </div>
     )
 }
