0959029322cc490e9d844a205bd41a75a8a209f5
FilipCrnoja <fc55256@fer.hr>
2025-01-17 13:42:55 +0100

    Baza + location

--- Frontend/react/src/My Page/ChangeLocation.jsx
+++ Frontend/react/src/My Page/ChangeLocation.jsx
@@ -1,9 +1,16 @@
+import {MapContainer, TileLayer, useMapEvents, Marker, Popup, useMap} from 'react-leaflet';
 
-export default function ChangeLocation() {
+export default function ChangeLocation(props) {
     return (
         <div>
-            <h3>Change location</h3>
-            
+            <h3>Lokacija ({props.location.latitude}, {props.location.longitude})</h3>
+            <MapContainer center={[props.location.latitude, props.location.longitude]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer 
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[props.location.latitude, props.location.longitude]} />
+            </MapContainer>
         </div>
     )
 }
--- Frontend/react/src/My Page/Postavke.jsx
+++ Frontend/react/src/My Page/Postavke.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import ChangeLocation from "./ChangeLocation";
+import axios from "axios";
 
 function Postavke() {
+
+    const [location, setLocation] = useState(null);
+    const [locationSet, setLocationSet] = useState(false)
+    const [auth, setAuth] = useState(JSON.parse(localStorage.getItem('auth')))
+
+    useEffect(() => {
+        //console.log("auth", auth);
+        if (location == null) {
+            axios.get('/api/users/username?username=' + auth.username)
+            .then(response => {
+                //console.log("Response", response.data);
+                setLocation({latitude:response.data.latitude, longitude:response.data.longitude});
+            })
+            .catch(error => {
+                console.log(error);
+            })
+        }
+    }, [auth])
+
+    useEffect(() => {
+        if (location) setLocationSet(true);
+    }, [location]);
+
+
     return (
         <>
             <h2>Postavke</h2>
             <hr />
-            <ChangeLocation />
+            {locationSet && <ChangeLocation location={location} />}
         </>
     )
 }
