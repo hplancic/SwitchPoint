3d7d2818f6420fe7a377d2970b032eadb498abd6
Sciller4 <mt55003@fer.hr>
2025-01-18 21:52:19 +0100

    Mijenjanje Lokacije

--- Frontend/react/src/My Page/ChangeLocation.jsx
+++ Frontend/react/src/My Page/ChangeLocation.jsx
@@ -1,16 +1,46 @@
 import {MapContainer, TileLayer, useMapEvents, Marker, Popup, useMap} from 'react-leaflet';
+import { useState } from 'react';
+import Map from "../Login and Signup/Map.jsx";
+import axios from "axios";
 
 export default function ChangeLocation(props) {
+    const initCoord = [props.location.latitude, props.location.longitude]
+    const [position, setPosition] = useState({"lat":initCoord[0], "lng":initCoord[1]});
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let username = JSON.parse(localStorage.getItem('auth')).username;
+        axios.get('/api/users/username?username=' + username)
+            .then(response => {
+                let userId = response.data.userId;
+                console.log(userId)
+                //console.log(artistAlbums, vinylIds);
+                axios.put('/api/users/' + userId + '/location?lat=' + position.lat + "&lon=" + position.lng
+                ) // + postPATH)
+                    .then(response => {
+                        console.log("Successfully changed location.");
+                        window.location.reload();
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+                
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
-        <div>
-            <h3>Lokacija ({props.location.latitude}, {props.location.longitude})</h3>
-            <MapContainer center={[props.location.latitude, props.location.longitude]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer 
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[props.location.latitude, props.location.longitude]} />
-            </MapContainer>
-        </div>
+        <>
+        <form action="post" onSubmit={handleSubmit}>
+            <h3>Promijeni Lokaciju:</h3>
+            <Map position={position} setPosition={setPosition} initCoord={initCoord} />
+            <h5>Zemljopisna Širina: {position.lat}</h5>
+            <h5>Zemljopisna Dužina: {position.lng}</h5>
+
+            <input type="submit" value="Promijeni" />
+        </form>
+        </>
     )
 }
